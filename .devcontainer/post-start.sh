28ab3f04ad97901010ecebe963c4de725362cd5c
Josh Abernathy <josh@github.com>
2022-06-02 23:12:55 -0400

    Delete post-start.sh

--- .devcontainer/post-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-curl https://joshabertest.blob.core.windows.net/dump/codespaces-liftoff-extension.vsix -o /tmp/codespaces-liftoff-extension.vsix
-code --install-extension /tmp/codespaces-liftoff-extension.vsix
